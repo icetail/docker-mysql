2a66c461be1343e41c540c5d45b46afcda2450ae
icetail <github@icenet.nl>
2021-02-26 03:56:47 +0100

    Create db_blocklist.sql

---
+++ .conf/sql/db_blocklist.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 24, 2021 at 12:05 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_blocklist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_blocklist`
+--
+
+CREATE TABLE `tbl_blocklist` (
+  `Id` int(11) NOT NULL,
+  `fldNaam` text NOT NULL,
+  `fldEmail` varchar(100) NOT NULL,
+  `fldTelefoon` varchar(100) NOT NULL,
+  `fldIpadress` varchar(500) NOT NULL,
+  `fldCallnr` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_blocklist`
+--
+ALTER TABLE `tbl_blocklist`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `ID` (`Id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
